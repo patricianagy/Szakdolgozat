a256f5ee7935f41f40cefd24afcf8dbb332e665d
patricianagy <nagy.patricia98@gmail.com>
2021-06-22 15:09:24 +0200

    tryed uot functions

--- próba/first_try/first_try/Source.cpp
+++ próba/first_try/first_try/Source.cpp
@@ -10,7 +10,18 @@ using namespace std;
 int main() {
 	string path = "Reasources/test.jpg";
 	Mat img = imread(path);
+	Mat imgGray;
+	Mat imgCan;
+
+	cvtColor(img, imgGray,COLOR_BGR2GRAY);
+	Canny(img, imgCan, 25, 75);
+
+
+
+
 	imshow("Image", img);
+	imshow("SZurke", imgGray);
+	imshow("ke", imgCan);
 	waitKey(0);
 	return 0;
 }
